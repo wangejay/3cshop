6593f4537ed22e33bc708705131af340b8f974c4
Jackie <wangejay@gmail.com>
2015-03-31 14:57:42 +0800

    email

--- wp-content/themes/startbiz/functions.php
+++ wp-content/themes/startbiz/functions.php
@@ -102,5 +102,17 @@ if ( !current_user_can('administrator') ) {
 	}</style>' ;
 }
 
+add_action('phpmailer_init', 'mail_smtp');
+function mail_smtp( $phpmailer ) {
+  $phpmailer->IsSMTP();
+  $phpmailer->SMTPAuth = true; // 啟用SMTPAuth服務
+  $phpmailer->Port = 465;  //SMTP郵件發送埠，
+  $phpmailer->SMTPSecure ="ssl"; //是否驗證ssl或tls
+  $phpmailer->Host = "smtp.gmail.com"; // 郵件的SMTP服務器地址
+  $phpmailer->Username = "wangejay@gmail.com<script cf-hash="f9e31" type="text/javascript">
+/* <![CDATA[ */!function(){try{var t="currentScript"in document?document.currentScript:function(){for(var t=document.getElementsByTagName("script"),e=t.length;e--;)if(t[e].getAttribute("cf-hash"))return t[e]}();if(t&&t.previousSibling){var e,r,n,i,c=t.previousSibling,a=c.getAttribute("data-cfemail");if(a){for(e="",r=parseInt(a.substr(0,2),16),n=2;a.length-n;n+=2)i=parseInt(a.substr(n,2),16)^r,e+=String.fromCharCode(i);e=document.createTextNode(e),c.parentNode.replaceChild(e,c)}}}catch(u){}}();/* ]]> */</script>"; //你的郵件地址
+  $phpmailer->Password ="zZ367172"; //你的郵件登陸密碼
+}
+
 
 ?>
